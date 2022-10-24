e339840d874cc4da7df39740dc06741d52166f6d
MartenMM <martenmiddeldorp@gmail.com>
2022-10-24 10:03:55 +0200

    Update MEGABLEND.R
    Updated to match current algorithm

--- MEGABLEND.R
+++ MEGABLEND.R
@@ -1,133 +1,209 @@
-library("tidyverse")
-library("lubridate")
-library("rvest")
-library("glue")
+setwd("D:/R/Muziek/Spotify")
 
-# Helper functions (local): source("spotify_funs.R")
-# Progress bar (local): source("progress_bar.R")
+suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("rvest"))
+suppressPackageStartupMessages(library("glue"))
 
+source('spotify_funs.R')
+source('D:/R/[code]/progress_bar.R')
+source("D:/R/Muziek/lastfm/lfm_get_toptracks.R")
 
-# Enter usernames for MEGABLEND
-usernames <- c("Multiple", "usernames", "here")
-
-# Exclude tracks that have been in MEGABLEND in the last 90 days
+usernames <- c("HiAndFi", "Maluki1", "KennedyJF", "Maaaaaaaaaax", "Karii-Anne", "jenny-oc", "Afraaaaahhh", "silvanbrummen", "MaggieMacster", "tplazarov")
 MEGABLEND_hist <- read_rds("MEGABLEND_hist.rds")
 
-remove <- MEGABLEND_hist %>% 
-  filter(date >= (today() - days(90)), date != today())
+# Remove tracks that have been added in the past 180 days:
+remove <- MEGABLEND_hist %>%
+  filter(date >= (today() - days(180)), date != today()) %>%
+  mutate(across(c(artist, track_title), tolower))
 
+# Calculate waiting period for artists that have been added in the past 90 days:
+wait <- MEGABLEND_hist %>%
+  filter(date >= (today() - days(90)), date != today()) %>%
+  group_by(artist = tolower(artist)) %>%
+  summarise(wait       = n(),
+            last_added = as_date(max(date)),
+            next_add   = last_added + weeks(wait),
+            .groups    = "drop") %>%
+  filter(next_add > today())
 
-
-# Load spotify history en matched scrobbles (both local)
+# Load locally cached data:
 load("play_history.RData")
-play_history <- raw %>% 
-  count(track.id) %>% 
+play_history <- raw %>%
+  count(track.id) %>%
   rename(playcount_spotify = n)
 matched <- map_dfr(c("match_exact", "match_full", "match_simple", "search_results"), function(x) {
   read_rds(glue("scrobbles/{x}.rds")) %>%
     mutate(match_type = ifelse(x == "search_results", "search", x))
-}) %>% 
-  left_join(play_history, by = c("track_id" = "track.id")) %>% 
+}) %>%
+  left_join(play_history, by = c("track_id" = "track.id")) %>%
   mutate(playcount_spotify = replace_na(playcount_spotify, 0))
-  
+
+track_info <- read_rds("cache/track_info.rds")
+album_info <- read_rds("cache/album_info.rds")
+artist_info <- read_rds("cache/artist_info.rds")
 
 
 
 
 
-# Import scrobbles from last.fm (last 90 days)
+# Import scrobbles from last.fm
 message("Loading scrobbles..")
 pb <- progress_bar(length(usernames))
-scrobbles <- map_dfr(usernames, function(username) {  
-  page1 <- read_html(glue("https://www.last.fm/user/{username}/library/tracks?date_preset=LAST_90_DAYS"))
-  page2 <- read_html(glue("https://www.last.fm/user/{username}/library/tracks?date_preset=LAST_90_DAYS&page=2"))
-  page3 <- read_html(glue("https://www.last.fm/user/{username}/library/tracks?date_preset=LAST_90_DAYS&page=3"))
-  page4 <- read_html(glue("https://www.last.fm/user/{username}/library/tracks?date_preset=LAST_90_DAYS&page=4"))
-  
-  # Rate limiting
-  Sys.sleep(3)
-  
-  # Progress
-  pb$tick()
-  
-  # Extract info from pages
-  map_dfr(list(page1, page2, page3, page4), function(html) {
-    tibble(
-      username = username,
-      track_name = html %>% 
-        html_nodes(".chartlist-name") %>% 
-        html_text(trim = TRUE), 
-      artist     = html %>% 
-        html_nodes(".chartlist-artist") %>% 
-        html_text(trim = TRUE),
-      n_scrobbles = html %>% 
-        html_nodes(".chartlist-count-bar-value") %>% 
-        html_text(trim = TRUE) %>% 
-        parse_number()
-    )
-  })
-})
+scrobbles <- map_dfr(usernames, function(username) {
 
-# Initial playlist: 
-# - Tracks that have not been added in last 90 days
-# - wt = (mean) relative importance for user = n_scrobbles / max(n_scrobbles)
-# - n_psn = number of users that have track in top-200 = n()
-# - Order (desc.) by wt*n_psn, tiebreaker: total number of scrobbles (n_scrobbles)
-# - Max. 1 track per (main) artist
-playlist <- scrobbles %>%
-  anti_join(remove, by = c("artist", "track_name")) %>%
+  scrobbles <- lfm_get_toptracks(username, period = "3month", top_n = 500)
+  pb$tick()
+
+  if (length(scrobbles) == 0) {
+    return(
+      NULL
+    )
+  }
+
+  scrobbles %>%
+    transmute(username = username,
+              order    = as.numeric(rank),
+              artist,
+              track_title,
+              scrobbles = as.numeric(playcount))
+}) %>%
+  distinct() %>%
   group_by(username) %>%
-  mutate(wt = n_scrobbles / max(n_scrobbles)) %>%
-  group_by(artist, track_name) %>%
+  mutate(wt = scrobbles / max(scrobbles)) %>%
+  ungroup()
+
+# Create 'raw' playlist:
+# > remove tracks in `remove` and `wait`
+# > calculate relative listening score
+# > sort by relative listening score, then by total number of scrobbles
+# > at most one track per artist
+playlist <- scrobbles %>%
+  mutate(across(c(artist, track_title), tolower)) %>%
+  anti_join(remove, by = c("artist", "track_title")) %>%
+  anti_join(wait, by = "artist") %>%
+  group_by(artist, track_title) %>%
   summarise(n_psn     = n(),
-            n_scrobbles = sum(n_scrobbles),
+            scrobbles = sum(scrobbles),
             wt        = mean(wt),
             .groups = "drop") %>%
-  arrange(desc(wt*n_psn), desc(n_scrobbles)) %>%
-  distinct(artist, .keep_all = TRUE)
+  arrange(desc(wt*n_psn), desc(scrobbles)) %>%
+  distinct(artist, .keep_all = TRUE) %>%
+  left_join(scrobbles %>%
+              mutate(across(c(artist, track_title), tolower)) %>%
+              group_by(artist, track_title) %>%
+              summarise(username = paste(username, collapse = ", "), .groups = "drop"),
+            by = c("artist", "track_title")) %>%
+  left_join(scrobbles %>%
+              mutate(across(c(artist, track_title), tolower)) %>%
+              select(username, artist, track_title, order),
+            by = c("username", "artist", "track_title")) %>%
+  arrange(desc(wt*n_psn), desc(scrobbles), order)
 
-# Multiple versions of the same track may exist 
-# Use 'my' version so my stats won't get messed up ;-)
+# Check if tracks in cached data have new track IDs:
 message("Getting new track_id's for matched tracks..")
-playlist_matched <- playlist %>% 
-  inner_join(matched, by = c("artist", "track_name" = "track_title")) %>% 
-  mutate(track_id_relinked = relink_track_info(track_id, album_id)) %>% 
-  mutate(track_id = ifelse(!is.na(track_id_relinked), track_id_relinked, track_id)) %>% 
-  group_by(artist, track_name) %>% 
-  slice_max(playcount_spotify, n = 1, with_ties = FALSE) %>% 
+playlist_matched <- playlist %>%
+  inner_join(matched %>%
+               mutate(across(c(artist, track_title), tolower)),
+             by = c("artist", "track_title" = "track_title")) %>%
+  mutate(track_id_relinked = relink_track_info(track_id, album_id)) %>%
+  mutate(track_id = ifelse(!is.na(track_id_relinked), track_id_relinked, track_id)) %>%
+  group_by(artist, track_title) %>%
+  slice_max(playcount_spotify, with_ties = FALSE) %>%
   ungroup()
-  
-# Use Spotify Search API if I haven't played track
-# Lookup at most 50 tracks to save time
+
+# Search track IDs for tracks that are not in cached data:
 message("Finding results for unmatched tracks..")
-search_results <- playlist %>% 
-  anti_join(playlist_matched, by = c("artist", "track_name")) %>% 
-  slice(1:pmin(50, n())) %>% 
-  mutate(query = glue('artist:"{artist}" track:"{track_name}"'),
+search_results <- playlist %>%
+  anti_join(playlist_matched, by = c("artist", "track_title")) %>%
+  slice(1:pmin(50, n())) %>%
+  mutate(query = glue('artist:"{artist}" track:"{track_title}"'),
          search_results = spotify_search(query, type = "track", nested = TRUE))
 
-# Not all tracks will return a result: 
-# search_results %>% 
-#   mutate(no_result = map_lgl(search_results, is_empty)) %>% 
-#   count(no_result)
+# Keep tracks that have a track ID:
+playlist_matched <- playlist_matched %>%
+  bind_rows(search_results %>%
+              unnest(search_results, names_repair = "universal") %>%
+              rename(track_title = track_title...2) %>%
+              group_by(artist, track_title) %>%
+              slice(1) %>%
+              ungroup() %>%
+              mutate(main_artist_id = map_chr(artists, ~ pull(., artist_id) %>% first)) %>%
+              select(artist, track_title, any_of(names(playlist_matched))))
 
-# Bind results and reorder for final playlist:
-playlist_def <- playlist_matched %>% 
-  bind_rows(search_results %>% 
-              unnest(search_results) %>% 
-              group_by(artist, track_name) %>% 
-              slice(1) %>% 
-              ungroup()) %>% 
-  arrange(desc(n_psn*wt), desc(n_scrobbles)) %>% 
-  slice(1:30) %>% 
-  select(artist:wt, track_id)
+# Exclude children's music
+excludes_genres <- playlist_matched %>%
+  pull(main_artist_id) %>%
+  subset(!is.na(.)) %>%
+  spotify_get_artists() %>%
+  filter(!map_lgl(genres, is_empty)) %>%
+  unnest(genres) %>%
+  group_by(main_artist_id = id) %>%
+  summarise(kinder = sum(str_detect(genres, c("(child)|(kind)")))) %>%
+  filter(kinder > 0) %>%
+  pull(main_artist_id)
 
-# Keep track of MEGABLEND history so tracks aren't recycled:
+
+
+# Initial playlist: thirty most-listened tracks that have at least two listeners
+playlist_multi <- playlist_matched %>%
+  filter(!main_artist_id %in% excludes_genres) %>%
+  arrange(desc(n_psn*wt), desc(scrobbles), order) %>%
+  slice(1:30) %>%
+  filter(n_psn > 1)
+
+# Calculate how many tracks a single listener can contribute:
+n_multi <- nrow(playlist_multi)
+n_per_person <- ceiling((30 - n_multi) / nrow(distinct(scrobbles, username))) + 1
+
+playlist_solo <- playlist_matched %>%
+  filter(!main_artist_id %in% excludes_genres) %>%
+  arrange(desc(n_psn*wt), desc(scrobbles), order) %>%
+  filter(n_psn == 1) %>%
+  group_by(username) %>%
+  slice_max(n_psn*wt, n = n_per_person, with_ties = FALSE) %>%
+  ungroup()
+
+# Add (max. `n_per_person`) tracks until 30 tracks are selected:
+playlist_def <- bind_rows(
+  playlist_multi,
+  playlist_solo
+) %>%
+  arrange(desc(n_psn*wt), desc(scrobbles), order) %>%
+  slice(1:30) %>%
+  select(artist:wt, track_id) %>%
+  left_join(scrobbles %>%
+              mutate(across(c(artist, track_title), tolower)) %>%
+              group_by(artist, track_title) %>%
+              summarise(username = paste(glue("{username} ({scrobbles})"), collapse = ", "), .groups = "drop"),
+            by = c("artist", "track_title"))
+
+# Write history to disk:
 bind_rows(MEGABLEND_hist %>% filter(date != today()),
           playlist_def %>% mutate(date = today())) %>%
   write_rds("MEGABLEND_hist.rds")
 
-# Update playlist using API:
+# Add tracks to playlist using API:
 playlist_def %>%
   pull(track_id) %>%
-  playlist_add_tracks(playlist_id = "2vB12PcJkkxxAkgOAueH32", remove_all_current = TRUE)
+  playlist_add_tracks(playlist_id = "2vB12PcJkkxxAkgOAueH32", remove_all_current = TRUE, verbose = FALSE)
+
+message("Done!")
+Sys.sleep(3)
+
+
+
+MEGABLEND_hist %>%
+  filter(date == today())
+
+# Restjes
+
+# gini <- scrobbles %>%
+#   anti_join(remove, by = c("artist", "track_title")) %>%
+#   group_by(username) %>%
+#   summarise(gini = DescTools::Gini(scrobbles))
+
+
+read_rds("MEGABLEND_hist.rds") %>%
+  filter(date == max(date)) %>%
+  select(artiest = artist, titel = track_title, scrobbles, aant_personen = n_psn, details = username) %>%
+  print(n = 30)
